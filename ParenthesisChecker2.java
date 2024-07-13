3c3cb9bc56449c4d4d21cd9ccc65f158ec8ee232
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2024-07-13 18:52:18 +0300

    Update and rename ParanthesisChecke2r.java to ParenthesisChecker2.java

--- ParenthesisChecker2.java
+++ ParenthesisChecker2.java
@@ -1,6 +1,6 @@
-public class ParanthesisChecker2 {
+public class ParenthesisChecker2 {
    public static boolean isBalanced(String expression) {
-   	int paranthesis = 0;
+   	int parenthesis = 0;
       int curlyBrace = 0;
       int bracket = 0;
         
@@ -8,13 +8,13 @@ public class ParanthesisChecker2 {
             char x = expression.charAt(i);
             
             if(x == '(') {
-                paranthesis++;
+                parenthesis++;
             } else if (x == '[') {
                 bracket++;
             } else if (x == '{') {
                 curlyBrace++;
             } else if (x == ')') {
-                paranthesis--;
+                parenthesis--;
             } else if (x == ']') {
                 bracket--;
             } else if (x == '}') {
@@ -23,7 +23,7 @@ public class ParanthesisChecker2 {
             
         }
         
-        if(paranthesis == 0 && curlyBrace == 0 && bracket == 0) {
+        if(parenthesis == 0 && curlyBrace == 0 && bracket == 0) {
             return true;
         } else {
             return false;
